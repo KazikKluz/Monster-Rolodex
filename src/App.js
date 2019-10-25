9bcedc69af3990e58f951b4cda914249c926df6b
Kazik Kluz <kazik.kluz@gmail.com>
2019-10-25 10:28:18 +0100

    1/2 custom components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      string: 'Kazik zrazik'
-    }
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: 1
+        },
+        {
+          name: "Banshee",
+          id: 2
+        },
+        {
+          name: "Black Smoke",
+          id: 3
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={()=>this.setState({string:'Karmapa Chenno!'})}>Change state!</button>
-          
-        </header>
+        <CardList monsters={this.state.monsters}/>>
       </div>
     );
   }
