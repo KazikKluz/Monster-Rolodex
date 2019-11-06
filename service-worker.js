1aa9188311944de72bda4a454bb28c1b82ba915f
Kazik Kluz <kazik.kluz@gmail.com>
2019-11-06 23:13:10 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https//:kazikkluz.github.io/Monster-Rolodex/precache-manifest.1a21d468d26fc5f069f22d85f20662b7.js"
+  "./precache-manifest.bf44670161a5bcf4baee376da458f8be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//:kazikkluz.github.io/Monster-Rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
