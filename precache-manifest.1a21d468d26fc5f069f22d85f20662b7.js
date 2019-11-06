79ddc43a3e12890cbf239f3fc378ee8bf7860d65
Kazik Kluz <kazik.kluz@gmail.com>
2019-11-06 18:54:54 +0000

    Updates

---
+++ precache-manifest.1a21d468d26fc5f069f22d85f20662b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa195ea46b1ee94e94a3e2d6a949120",
+    "url": "https//:kazikkluz.github.io/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "c604fa9d7c6211fd4d6f",
+    "url": "https//:kazikkluz.github.io/Monster-Rolodex/static/css/main.6676a9f0.chunk.css"
+  },
+  {
+    "revision": "6f2bbdfca0a4472dec26",
+    "url": "https//:kazikkluz.github.io/Monster-Rolodex/static/js/2.dfe4b3bc.chunk.js"
+  },
+  {
+    "revision": "c604fa9d7c6211fd4d6f",
+    "url": "https//:kazikkluz.github.io/Monster-Rolodex/static/js/main.2c2209bf.chunk.js"
+  },
+  {
+    "revision": "bbb21d6b3c1c88a11861",
+    "url": "https//:kazikkluz.github.io/Monster-Rolodex/static/js/runtime-main.8d515fae.js"
+  }
+]);
