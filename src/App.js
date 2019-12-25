ab90eb8514bbfbfcd9158b0b7d8d451a169ccda9
Kazik Kluz <kazik.kluz@gmail.com>
2019-12-25 19:23:09 +0000

    29 card component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: []
-    }
+    };
 
   }
 
@@ -24,17 +24,10 @@ class App extends Component {
 
 
       }
-  
-
-
-
   render(){
     return (
       <div className="App">
-          <CardList>
-              {this.state.monsters.map(
-                  monster=>(<h1 key={monster.id}>{monster.name}</h1>))}
-          </CardList>
+          <CardList monsters={this.state.monsters} />
       </div>
     );
   }
