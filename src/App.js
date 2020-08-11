d473fe5e65b546e4d239b40e847f2b13c210e581
KazikKluz <kazik.kluz@gmail.com>
2020-08-11 12:01:18 +0100

    created card list component to render future cards 28

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+
+import CardList from "./components/card-list/card-list.component";
 
 import "./App.css";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
       monsters: [
-        {name: "Frankenstein", id: 1},
-        {name: "Black Smoke", id: 2},
-        {name: "Chaka Zulu", id: 3},
-        {name: "It", id: 4},
-        {name: "Godzilla", id: 5},
-        {name: "Big Fat Guy", id: 6}
-      ]
-    }
-  
+        { name: "Frankenstein", id: 1 },
+        { name: "Black Smoke", id: 2 },
+        { name: "Chaka Zulu", id: 3 },
+        { name: "It", id: 4 },
+        { name: "Godzilla", id: 5 },
+        { name: "Big Fat Guy", id: 6 },
+      ],
+    };
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster=>
-          <h1 id={monster.id}>{monster.name}</h1>
-          )}
+        <CardList name="Kazik">
+          {this.state.monsters.map((monster) => (
+            <h1 id={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
-  
 }
 
 export default App;
